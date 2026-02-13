ad12961879fd7ec8f76e9516e6e7be291de92925
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-02-13 13:52:26 -0300

    improving prompt

--- ollama_assistant.py
+++ ollama_assistant.py
@@ -20,13 +20,20 @@ class OllamaMemoryAssistant:
         else:
             context = "No relevant notes found."
 
-        prompt = prompt = f"""
+        prompt = f"""
         You are a personal memory assistant.
         Answer questions based on the user's notes below.
         {context}
         Question: {question}
-        Answer based only on the notes provided.
-        If the answer isn't in the notes, say so politely."""
+        Rules:
+            - Answer based only on the notes provided.
+            - Do not try to guess the meaning of the note if ambiguous,
+        just mention the note.
+            - The notes is in markdown, use this to filter context by title when needed.
+            - Do not make suggestion unless requested.
+            - If handling a checklist: [x] means completed and [ ] means pending.
+            - If the answer isn't in the notes, say so politely.
+            """
 
         response = requests.post(self.ollama_url, json={
             "model": self.model,
